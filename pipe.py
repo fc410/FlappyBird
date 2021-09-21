c2aa89bc3c9d87e81bc466d30463890233bd430d
fc410 <fc410@humboldt.edu>
2021-09-21 02:12:37 -0700

    Created the pipe class

--- pipe.py
+++ pipe.py
@@ -1,40 +1,65 @@
 import pygame
 import random
-import functions
-import variableDatabase
-import images
 
 pygame.init()
+screen = pygame.display.set_mode((574, 800))
 
 
-def create_pipe():
-    random_pipe_pos = random.choice(pipe_height)
-    bottom_pipe = pipe_surface.get_rect(midtop=(700, random_pipe_pos))
-    top_pipe = pipe_surface.get_rect(midbottom=(700, random_pipe_pos - 250))
-    return bottom_pipe, top_pipe
+class Pipe:
+    # A list to put all the pipes in for handling in the future
+    pipe_list = []
 
+    def __init__(self, pipes):
+        # Initiates the given list of pipes to the objects pipe_list
+        self.pipe_list = pipes
 
-def move_pipes(pipes, s):
-    for pip in pipes:
-        pip.centerx -= 5
-        if pip.centerx <= 400:
-            s += 1
-    return pipes
+    @staticmethod
+    def draw_pipes(pipes, pipe_surface):
+        """
+        This function draws the given pipe surface in the given location
+        of the pipe list
+        :param pipes: I list of pipes that have been created already
+        :param pipe_surface: I pipe surface or image loaded in using the pygame
+                             library
+        :return: two pipes on inverted as the other to make a small window
+                 where the bird can fly through
+        """
+        for pipe in pipes:
+            if pipe.bottom >= 800:
+                screen.blit(pipe_surface, pipe)
+            else:
+                flip_pipe = pygame.transform.flip(pipe_surface, False, True)
+                screen.blit(flip_pipe, pipe)
 
+    @staticmethod
+    def create_pipe(pipe_surface, pipe_height):
+        """
+        This function creates a pipe from the given pipe surface or image
+        loaded in using the pygame library with a random height from the given
+        pipe_height list
+        :param pipe_surface: An image loaded in using the pygame library
+        :param pipe_height: A pipe height list to randomly choose from
+        :return: returns a bottom pipe with a random height from the given pipe
+                 height list and a top pipe from the random pipe height - 250
+                 to give the bird a small window to jump through
+        """
+        random_pipe_pos = random.choice(pipe_height)
+        bottom_pipe = pipe_surface.get_rect(midtop=(700, random_pipe_pos))
+        top_pipe = pipe_surface.get_rect(midbottom=(700, random_pipe_pos - 250))
+        return bottom_pipe, top_pipe
 
-def draw_pipes(pipes):
-    for pip in pipes:
-        if pip.bottom >= 800:
-            images.screen.blit(pipe_surface, pip)
-        else:
-            flip_pipe = pygame.transform.flip(pipe_surface, False, True)
-            images.screen.blit(flip_pipe, pip)
-
-
-# importing pipe images
-pipe_surface = pygame.image.load('assets/pipe-green.png')
-pipe_surface = pygame.transform.scale2x(pipe_surface)
-pipe_list = []
-SPAWNPIPE = pygame.USEREVENT
-pygame.time.set_timer(SPAWNPIPE, 1200)
-pipe_height = [300, 400, 600]
+    @staticmethod
+    def move_pipes(pipes, score):
+        """
+        This function moves the pipes at 5 pixels per frames to make its
+        seem like the pipes are moving and increases the score if the pipes
+        is less than or equal to 300 pixels
+        :param pipes: A list of pipes to move all the pipes in the list
+        :param score: An integer value that shows the current score of the player
+        :return: Returns the pipe list with all pipes moved 5 pixels to the left
+        """
+        for pipe in pipes:
+            pipe.centerx -= 5
+            if pipe.centerx <= 300:
+                score += 1
+        return pipes
